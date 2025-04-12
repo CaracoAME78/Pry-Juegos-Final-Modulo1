0f6293b75b575e02578b98dd6cd21442db90c08c
Carlos Ramos <carlosrc78@gmail.com>
2025-04-11 23:07:17 -0500

    Agregar archivos de proyecto.

---
+++ Pry-Juegos-Final-Modulo1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Pry_Juegos_Final_Modulo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
